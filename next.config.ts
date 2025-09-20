b79981ddcb6163096e1754eba5527d4bf3059f0c
Adam Loos <samthedog2k2@gmail.com>
2025-09-20 01:01:14 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -33,8 +33,8 @@ const nextConfig: NextConfig = {
   },
   ...(process.env.NODE_ENV === 'development' && {
       experimental: {
-          allowedDevOrigins: ['*'],
-      }
+      },
+      allowedDevOrigins: ['*'],
   })
 };
 
