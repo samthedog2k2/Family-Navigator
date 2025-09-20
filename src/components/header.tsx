1cffe0ab40a8db71b55f8e06ae379b397afb30e6
Adam Loos <samthedog2k2@gmail.com>
2025-09-20 01:04:35 +0000

    That icon is terrible - make a fancy N/M and not a weird picture

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,16 @@
 
 import Link from "next/link";
-import { Layers } from "lucide-react";
 import { MainNav } from "@/components/main-nav";
 import { UserNav } from "@/components/user-nav";
 import { cn } from "@/lib/utils";
+import { Logo } from "@/components/logo";
 
 export function Header({ className }: { className?: string}) {
   return (
     <header className={cn("sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60", className)}>
       <div className="container flex h-14 items-center">
         <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Layers className="h-6 w-6" />
+          <Logo className="h-6 w-6" />
           <span className="font-bold sm:inline-block">
             Family Navigator
           </span>
